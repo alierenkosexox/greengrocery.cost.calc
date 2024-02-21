02500e5cec8244a9db6c0fd16450f2d2f94dc5f5
Ali Eren KÖSE <alierenkose@gmail.com>
2024-02-21 06:44:29 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
-}
+        double pearP = 2.14, appleP=3.67 , tomatoP=1.11 , bananaP = 0.95 , eggplantP= 0.95, totalcost=0;
+        //products and prices
+
+        System.out.println("-->> WELCOME TO PATİKA GREENGROCERY <<--"); //price list and welcome
+        System.out.println("----------------------------------------");
+        System.out.println("Price list:");
+        System.out.println("Pear Price: 2.14");
+        System.out.println("Apple Price 3.67");
+        System.out.println("Tomatoes Price:  1.11");
+        System.out.println("Banana Price 0.95");
+        System.out.println("Eggplant Price 5.0");
+        System.out.println(",-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-");
+
+        double pearkg,applekg,tomatokg,bananakg,eggplantkg; //how many kilos were sold info
+
+        Scanner input = new Scanner(System.in);  //collecting weight info
+
+        System.out.print("Pear kg : ");
+        pearkg = input.nextDouble();            //input pear kg
+        totalcost +=pearkg*pearP;
+
+        System.out.print("Apple kg : ");
+        applekg = input.nextDouble();           //input apple kg
+        totalcost +=applekg*appleP;
+
+        System.out.print("Tomato kg : ");
+        tomatokg = input.nextDouble();              //input tamato kg
+        totalcost +=tomatokg*tomatoP;
+
+        System.out.print("Banana kg : ");
+        bananakg = input.nextDouble();              //input banana kg
+        totalcost +=bananakg*bananaP;
+
+        System.out.print("Eggplant kg : ");
+        eggplantkg = input.nextDouble();               //input eggplant kg
+        totalcost += eggplantkg*eggplantP;
+
+        System.out.println("Total price :"+ totalcost);   //write the total price
+
+    }                                         //Ölüm ve yaşam ikisinin de bir nedeni yoktur\\
+}  // Ali Eren KÖSE 21/02/2024 06:37        //  İnsanlar doğar çünkü annler bebek ister,    \\
+                                        //ya da tanrı safında savaşacak savaşçıya ihtiyaç vardır.\\
